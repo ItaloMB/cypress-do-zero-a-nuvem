fffca6925fb42b2fad70d9fa36b771b20a28ff35
ItaloMB <italomartins741@gmail.com>
2026-06-25 18:03:57 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId:'7ws137',
+  projectId: "uyzbvp",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
